cc881b33b4afe0e35907f5bbc3b147563c364296
100nout.by <dzmiter2105@gmail.com>
2023-05-30 15:08:25 +0300

    Basic JavaScript 36/113

--- script.js
+++ script.js
@@ -40,3 +40,117 @@ const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
+
+// ================ 11
+const sum = 10 + 10;
+
+// ================ 12
+const difference = 45 - 33;
+
+// ================ 13
+const product = 8 * 10;
+
+// ================ 14
+const quotient = 66 / 33;
+
+// ================ 15
+// let myVar = 87;
+// myVar++;
+
+// ================ 16
+let myVar = 11;
+myVar--;
+
+// ================ 17
+const ourDecimal = 5.7;
+let myDecimal = 5.7;
+
+// ================ 18
+// const product = 2.0 * 2.5;
+
+// ================ 19
+// const quotient = 4.4 / 2.0;
+
+// ================ 20
+const remainder = 11 % 3;
+
+// ================ 21
+let a = 3;
+let b = 17;
+let c = 12;
+a += 12;
+b += 9;
+c += 7;
+
+// ================ 22
+let a = 11;
+let b = 9;
+let c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
+
+// ================ 23
+let a = 5;
+let b = 12;
+let c = 4.6;
+a *= 5;
+b *= 3;
+c *= 10;
+
+// ================ 24
+let a = 48;
+let b = 108;
+let c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+
+// ================ 25
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// ================ 26
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// ================ 27
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+// ================ 28
+// const myStr = "This is the start." + " This is the end.";
+
+// ================ 29
+// let myStr = "This is the first sentence.";
+// myStr += " This is the second sentence.";
+
+// ================ 30
+// const myName = "Sashka";
+// const myStr = "My name is " + myName + " and I am well!";
+
+// ================ 31
+const someAdjective = "good";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// ================ 32
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
+// lastNameLength = lastName.length;
+
+// ================ 33
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
+// firstLetterOfLastName = lastName[0];
+
+// ================ 34
+// let myStr = "Jello World";
+// myStr = "Hello World"; 
+
+// ================ 35
+// const lastName = "Lovelace";
+// const thirdLetterOfLastName = lastName[2];
+
+// ================ 36
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1];

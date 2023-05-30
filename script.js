12759167f2b5384815f1f5cb39abfd6eb68b22be
100nout.by <dzmiter2105@gmail.com>
2023-05-30 09:18:27 +0300

    Basic JavaScript 10/113

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-// ================ 1
+// ================ 3
 var a;
 a = 7;
 
-// ================ 2
+// ================ 4
 var a;
 a = 7;
 var b;
 b = a;
+
+// ================ 5
+var a = 9;
+
+// ================ 6
+var myFirstName = "Vasya";
+var myLastName = "Pupkin";
+
+// ================ 7
+var a = 5;
+var b = 10;
+var c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// ================ 8
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// ================ 9
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// ================ 10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line

3de51944ea93cbe6bb5c51b0fc03ea745104f63b
100nout.by <dzmiter2105@gmail.com>
2023-06-02 08:36:47 +0300

    Basic JavaScript 70/113

--- script.js
+++ script.js
@@ -308,3 +308,105 @@ function testEqual(val) {
     return "Not Equal";
 }
 testEqual(10);
+
+// ================ 61
+function testStrict(val) {
+  if (val === 7) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+testStrict(10);
+
+// ================ 62
+function compareEquality(a, b) {
+  if (a === b) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+compareEquality(10, "10");
+
+// ================ 63
+function testNotEqual(val) {
+  if (val != 99) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testNotEqual(10);
+
+// ================ 64
+function testStrictNotEqual(val) {
+  if (val !== 17) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testStrictNotEqual(10);
+
+// ================ 65
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+  if (val > 10) {  
+    return "Over 10";
+  }
+  return "10 or Under";
+}
+testGreaterThan(10);
+
+// ================ 66
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  
+    return "20 or Over";
+  }
+  if (val >= 10) { 
+    return "10 or Over";
+  }
+  return "Less than 10";
+}
+testGreaterOrEqual(10);
+
+// ================ 67
+function testLessThan(val) {
+  if (val < 25) {  
+    return "Under 25";
+  }
+  if (val < 55) {  
+    return "Under 55";
+  }
+  return "55 or Over";
+}
+testLessThan(10);
+
+// ================ 68
+function testLessOrEqual(val) {
+  if (val <= 12) {  
+    return "Smaller Than or Equal to 12";
+  }
+  if (val <= 24) {  
+    return "Smaller Than or Equal to 24";
+  }
+  return "More Than 24";
+}
+testLessOrEqual(10);
+
+// ================ 69
+function testLogicalAnd(val) {
+  if (val >= 25 && val <= 50) {
+    return "Yes";
+  }
+  return "No";
+}
+testLogicalAnd(10);
+
+// ================ 70
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+testLogicalOr(15);

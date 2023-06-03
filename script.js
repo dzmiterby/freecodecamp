d60f2d54fde9c59c09de26169f8972f1995fd15b
100nout.by <dzmiter2105@gmail.com>
2023-06-03 08:18:15 +0300

    Basic JavaScript 80/113

--- script.js
+++ script.js
@@ -410,3 +410,170 @@ function testLogicalOr(val) {
   return "Inside";
 }
 testLogicalOr(15);
+
+// ================ 71
+function testElse(val) {
+  let result = "";
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+  return result;
+}
+testElse(4);
+
+// ================ 72
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+testElseIf(7);
+
+// ================ 73
+function orderMyLogic(val) {
+  if (val < 10 && val >= 5) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(7);
+
+// ================ 74
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num >= 5 && num < 10) {
+    return "Small";
+  } else if (num >= 10 && num < 15) {
+    return "Medium";
+  } else if (num >= 15 && num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+}
+testSize(7);
+
+// ================ 75
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3	) {
+    return "Go Home!";
+  }
+}
+golfScore(5, 4);
+
+// ================ 76
+function caseInSwitch(val) {
+  let answer = "";
+  switch (val) {
+    case 1: 
+      answer = "alpha";
+      break;
+    case 2: 
+      answer = "beta";
+      break;
+    case 3: 
+      answer = "gamma";
+      break;
+    case 4: 
+      answer = "delta";
+      break;
+  }
+  return answer;
+}
+caseInSwitch(1);
+
+// ================ 77
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+    case "a": 
+      answer = "apple";
+      break;
+    case "b": 
+      answer = "bird";
+      break;
+    case "c": 
+      answer = "cat";
+      break;
+    default: 
+      answer = "stuff";
+      break;
+  }
+  return answer;
+}
+switchOfStuff("a");
+
+// ================ 78
+function sequentialSizes(val) {
+  let answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+  }
+  return answer;
+}
+sequentialSizes(1);
+
+// ================ 79
+function chainToSwitch(val) {
+  let answer = "";
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+  }
+  return answer;
+}
+chainToSwitch(7);
+
+// ================ 80
+function isLess(a, b) {
+  return a < b;
+}
+isLess(10, 15);

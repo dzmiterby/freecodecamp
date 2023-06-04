7b007f89f772877442b066da7a15754122f7c29c
100nout.by <dzmiter2105@gmail.com>
2023-06-04 11:07:54 +0300

    Basic JavaScript 90/113

--- script.js
+++ script.js
@@ -632,10 +632,63 @@ const myDog = {
 // const shirtValue = testObj.shirt; 
 
 // ================ 85
+// const testObj = {
+//   "an entree": "hamburger",
+//   "my side": "veggies",
+//   "the drink": "water"
+// };
+// const entreeValue = testObj["an entree"];  
+// const drinkValue = testObj["the drink"]; 
+
+// ================ 86
 const testObj = {
-  "an entree": "hamburger",
-  "my side": "veggies",
-  "the drink": "water"
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
 };
-const entreeValue = testObj["an entree"];  
-const drinkValue = testObj["the drink"]; 
+const playerNumber = 16;
+const player = testObj[playerNumber];
+
+// ================ 87
+// const myDog = {
+//   "name": "Coder",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["freeCodeCamp Campers"]
+// };
+// myDog.name = "Happy Coder";
+
+// ================ 88 
+// const myDog = {
+//   "name": "Happy Coder",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["freeCodeCamp Campers"]
+// };
+// myDog.bark = "woof";
+
+// ================ 89
+// const myDog = {
+//   "name": "Happy Coder",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["freeCodeCamp Campers"],
+//   "bark": "woof"
+// };
+// delete myDog.tails;
+
+// ================ 90
+function phoneticLookup(val) {
+  let result ="";
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+  result = lookup[val];
+  return result;
+}
+phoneticLookup("charlie");

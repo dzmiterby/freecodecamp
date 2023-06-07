a6fd2325126835ef67cec6eb0430e406c5552b96
100nout.by <dzmiter2105@gmail.com>
2023-06-07 16:47:05 +0300

    Regular Expressions 5/33

--- script.js
+++ script.js
@@ -1211,3 +1211,29 @@ const makeServerRequest = new Promise((resolve, reject) => {
 // makeServerRequest.catch(error => {
 //   console.log(error);
 // });
+
+// ================ Regular Expressions ================
+// ================ 1
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString);
+
+// ================ 2
+// let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+// let waldoRegex = /Waldo/;
+// let result = waldoRegex.test(waldoIsHiding);
+
+// ================ 3
+// let petString = "James has a pet cat.";
+// let petRegex = /dog|cat|bird|fish/;
+// let result = petRegex.test(petString);
+
+// ================ 4
+// let myString = "freeCodeCamp";
+// let fccRegex = /freeCodeCamp/i; // Change this line
+// let result = fccRegex.test(myString);
+
+// ================ 5
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line

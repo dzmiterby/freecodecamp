2ef8ca8dde50434c64fa45484f712e7b27642cc6
100nout.by <dzmiter2105@gmail.com>
2023-06-07 08:50:16 +0300

    ES6 29/29

--- script.js
+++ script.js
@@ -1134,3 +1134,80 @@ temp = thermos.temperature;
     <script type="module" src="index.js"></script>
   </body>
 </html>
+
+// ================ 21
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+export { uppercaseString, lowercaseString };
+
+// ================ 22
+import { uppercaseString, lowercaseString } from './string_functions.js';
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+// ================ 23
+import * as stringFunctions from "./string_functions.js";
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+// ================ 24
+export default function subtract(x, y) {
+  return x - y;
+}
+
+// ================ 25
+import subtract from "./math_functions.js";
+subtract(7,4);
+
+// ================ 26
+const makeServerRequest = new Promise((resolve, reject) => {
+  let responseFromServer;
+  if(responseFromServer) {
+     resolve("We got the data");
+  } else {  
+     reject("Data not received");
+  }
+})
+
+// ================ 27
+// const makeServerRequest = new Promise((resolve, reject) => {
+//   let responseFromServer = true;
+//   if(responseFromServer) {
+//     resolve("We got the data");
+//   } else {  
+//     reject("Data not received");
+//   }
+// })
+
+// ================ 28
+// const makeServerRequest = new Promise((resolve, reject) => {
+//   let responseFromServer = true;
+//   if(responseFromServer) {
+//     resolve("We got the data");
+//   } else {  
+//     reject("Data not received");
+//   }
+// });
+// makeServerRequest.then(result => {
+//   console.log(result);
+// });
+
+// ================ 29
+// const makeServerRequest = new Promise((resolve, reject) => {
+//   let responseFromServer = false;
+//   if(responseFromServer) {
+//     resolve("We got the data");
+//   } else {  
+//     reject("Data not received");
+//   }
+// });
+// makeServerRequest.then(result => {
+//   console.log(result);
+// });
+// makeServerRequest.catch(error => {
+//   console.log(error);
+// });

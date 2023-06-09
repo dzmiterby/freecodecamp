678b13cc1eef4f41d6cb163f310442ef98b0a65d
100nout.by <dzmiter2105@gmail.com>
2023-06-09 16:52:38 +0300

    Debugging 6/12

--- script.js
+++ script.js
@@ -1376,3 +1376,38 @@ let reCriminals = /C+/;
 // let hello = "   Hello, World!  ";
 // let wsRegex = /^(\s+)(.+[^\s])(\s+)$/;
 // let result = hello.replace(wsRegex, '$2');
+
+// ================ Debugging ================
+// ================ 1
+let a = 5;
+let b = 1;
+a++;
+let sumAB = a + b;
+console.log(a);
+
+// ================ 2
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
+
+// ================ 3
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof seven);
+console.log(typeof three);
+
+// ================ 4
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+// ================ 5
+// let myArray = [1, 2, 3];
+// let arraySum = myArray.reduce((previous, current) =>  previous + current);
+// console.log(`Sum of array values is: ${arraySum}`);
+
+// ================ 6
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);

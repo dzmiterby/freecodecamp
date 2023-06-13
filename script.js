d8f22a2028720a16fc78c16c54458b72a55ea6db
100nout.by <dzmiter2105@gmail.com>
2023-06-13 08:53:04 +0300

    Basic Data Structures 19/20

--- script.js
+++ script.js
@@ -1681,3 +1681,27 @@ console.log(userActivity);
 //   return count;
 // }
 // console.log(countOnline(users));
+
+// ================ 19
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+}
+function getArrayOfUsers(obj) {
+  return Object.keys(obj);
+}
+console.log(getArrayOfUsers(users));
